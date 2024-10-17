29049780d01dece26418fc4fb352d1dd2de46d0c
SonyPpv <sonyappv@gmail.com>
2024-10-17 11:11:18 +0300

    Add files via upload

---
+++ 11.bat
@@ -0,0 +1,5 @@
+@Echo OFF
+set /P pyt1=enter papka1 
+set /P pyt2=enter papka2 
+XCOPY %pyt1% %pyt2% /S
+pause
---
+++ 5555.bat
@@ -0,0 +1,7 @@
+@Echo off
+set /P ch1=enter 1 ch
+set /P ch2=enter 2 ch
+set /A c=%ch1%+%ch2%
+Echo otvet:%c%
+pause
+
